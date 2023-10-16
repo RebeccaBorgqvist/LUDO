bdca529e47e9cd7a1471c61e6137050d8d0dcdbe
Rebecca Borgqvist <rebeccaborgqvist@gmail.com>
2023-10-16 16:17:52 +0200

    Navigation correction in App.xaml.cs so it goes to MainMenu again

--- App.xaml.cs
+++ App.xaml.cs
@@ -67,7 +67,7 @@ namespace LUDO
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(GameBoard), e.Arguments); // GLÖM INTE ÄNDRA TILLBAKA TILL MAINMENU
+                    rootFrame.Navigate(typeof(MainMenu), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
