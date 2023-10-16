f20a30d631b9addcb7f24b76a8423539b5f6d736
Gabriella <gabivrban@gmail.com>
2023-10-16 14:49:40 +0200

    Updates

--- ViewModels/GameSettingsViewModel.cs
+++ ViewModels/GameSettingsViewModel.cs
@@ -16,6 +16,29 @@ namespace LUDO.ViewModels
         private bool _isPlayer4Visible;
         private bool _isMinusButtonVisible;
         private bool _isPlusButtonVisible = true;
+        private bool _isPlayer3ColorVisible;
+        private bool _isPlayer4ColorVisible;
+
+        private bool _blue1 = true;
+        private bool _blue2 = true;
+        private bool _blue3 = true;
+        private bool _blue4 = true;
+
+        private bool _red1 = true;
+        private bool _red2 = true;
+        private bool _red3 = true;
+        private bool _red4 = true;
+
+        private bool _green1 = true;
+        private bool _green2 = true;
+        private bool _green3 = true;
+        private bool _green4 = true;
+
+        private bool _yellow1 = true;
+        private bool _yellow2 = true;
+        private bool _yellow3 = true;
+        private bool _yellow4 = true;
+
         private string _addRemoveText = "Add more players";
         private int _players = 2;
         private string _player1Name = "Player 1";
@@ -26,6 +49,7 @@ namespace LUDO.ViewModels
         public ICommand MinusPlayerCommand { get; set; }
         public ICommand ForwardCommand { get; set; }
         public ICommand ChangeNameCommand { get; set; }
+        public ICommand ChangeColor { get; set; }
         public static GameSettingsViewModel Instance { get; set; }
         public bool IsPlayer3Visible
         {
@@ -113,6 +137,173 @@ namespace LUDO.ViewModels
             get { return _players; }
             set { _players = value; }
         }
+        public bool IsPlayer3ColorsVisible
+        {
+            get { return _isPlayer3ColorVisible; }
+            set
+            {
+                _isPlayer3ColorVisible = value;
+                OnPropertyChanged(nameof(IsPlayer3ColorsVisible));
+            }
+        }
+
+        public bool IsPlayer4ColorsVisible
+        {
+            get { return _isPlayer4ColorVisible; }
+            set
+            {
+                _isPlayer4ColorVisible = value;
+                OnPropertyChanged(nameof(IsPlayer4ColorsVisible));
+            }
+        }
+        // Blue
+        public bool Blue1
+        {
+            get { return _blue1; }
+            set
+            {
+                _blue1 = value;
+                OnPropertyChanged(nameof(Blue1));
+            }
+        }
+
+        public bool Blue2
+        {
+            get { return _blue2; }
+            set
+            {
+                _blue2 = value;
+                OnPropertyChanged(nameof(Blue2));
+            }
+        }
+        public bool Blue3
+        {
+            get { return _blue3; }
+            set
+            {
+                _blue3 = value;
+                OnPropertyChanged(nameof(Blue3));
+            }
+        }
+        public bool Blue4
+        {
+            get { return _blue4; }
+            set
+            {
+                _blue4 = value;
+                OnPropertyChanged(nameof(Blue4));
+            }
+        }
+        public bool Red1
+        {
+            get { return _red1; }
+            set
+            {
+                _red1 = value;
+                OnPropertyChanged(nameof(Red1));
+            }
+        }
+        public bool Red2
+        {
+            get { return _red2; }
+            set
+            {
+                _red2 = value;
+                OnPropertyChanged(nameof(Red2));
+            }
+        }
+        public bool Red3
+        {
+            get { return _red3; }
+            set
+            {
+                _red3 = value;
+                OnPropertyChanged(nameof(Red3));
+            }
+        }
+        public bool Red4
+        {
+            get { return _red4; }
+            set
+            {
+                _red4 = value;
+                OnPropertyChanged(nameof(Red4));
+            }
+        }
+        public bool Green1
+        {
+            get { return _green1; }
+            set
+            {
+                _green1 = value;
+                OnPropertyChanged(nameof(Green1));
+            }
+        }
+        public bool Green2
+        {
+            get { return _green2; }
+            set
+            {
+                _green2 = value;
+                OnPropertyChanged(nameof(Green2));
+            }
+        }
+        public bool Green3
+        {
+            get { return _green3; }
+            set
+            {
+                _green3 = value;
+                OnPropertyChanged(nameof(Green3));
+            }
+        }
+        public bool Green4
+        {
+            get { return _green4; }
+            set
+            {
+                _green4 = value;
+                OnPropertyChanged(nameof(Green4));
+            }
+        }
+
+        public bool Yellow1
+        {
+            get { return _yellow1; }
+            set
+            {
+                _yellow1 = value;
+                OnPropertyChanged(nameof(Yellow1));
+            }
+        }
+        public bool Yellow2
+        {
+            get { return _yellow2; }
+            set
+            {
+                _yellow2 = value;
+                OnPropertyChanged(nameof(Yellow2));
+            }
+        }
+        public bool Yellow3
+        {
+            get { return _yellow3; }
+            set
+            {
+                _yellow3 = value;
+                OnPropertyChanged(nameof(Yellow3));
+            }
+        }
+        public bool Yellow4
+        {
+            get { return _yellow4; }
+            set
+            {
+                _yellow4 = value;
+                OnPropertyChanged(nameof(Yellow4));
+            }
+        }
+
         public GameSettingsViewModel()
         {
             Instance = this;
@@ -120,6 +311,8 @@ namespace LUDO.ViewModels
             MinusPlayerCommand = new RemovePlayerCommand();
             ForwardCommand = new ForwardCommand();
             ChangeNameCommand = new ChangeNameCommand();
+            ChangeColor = new PlayersSelectedColorCommand();
         }
+
     }
 }
